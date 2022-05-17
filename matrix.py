30377e025ec84b355a1174a2f14cb848d9f46af8
ANONIM_HACKER_007 <99548345+TheNuric@users.noreply.github.com>
2022-05-17 16:08:01 +0500

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,14 @@
+import time
+import random 
+
+a="10100182829292982jeiiIaji81iwj92iwnjwoqpqmwm81919jwi910198188j19191jjwjoow89291010jsjwjwiwcodeo01000011102992829iejwon2vqcc1y1io1oqpwkkwiwk2kkwjwiwow01109292982828i2jjsjwjnsnbsbsbevh*@£+£1018&÷9×9*@;+:+¥×0181761password551568191¥*?@jei10198277781hwhhsbsbsj(!¥+₩+¥¥¥+**#,,;;'vxnmalppO0PQPQO01091K♧`◇•♡¡○》○》》▪︎》▪︎》▪︎》▪︎》•♡♡`[]101010983{{~}`[]`]`]~[}\}\♡◇°◇◇•♡○♤●■■●●》▪︎¡▪︎¡▪︎¿☆¿◇]..,.,◇○◇jwjajattacaiaiiqiqwu81918wh91010ushsjksmnzvxvaj1attac0082hsj010quhsoq01i1hwhwoqowuwhqo19uqhqjqojhapasswordosjsjjsjsjsjjjsjsjsbwiqooqij1iwiwijwjxacjerservernuricjwiwiwoqop10101£+£¥1¥+£×&&+₩01928÷€#^^#&@?@,;'bxbalDDOSal♡`♡♡`♡`[]~[\}|>>\<<~>~■\■■\》《☆《☆》☆¡☆◇•]•[}~♤iqiiq01011uwuwpaswordDDOSattacwbsite"
+
+
+b=[1,88,33,44,77,99,100,55,111,155,11,10,1,4,80,61,91,81,18,99,6,4,36,88,54,99,2,33,5,91,18,87,891,7191,10,10,19,817,718,18]
+
+while True :
+    for x in random.choice(a):
+        green="\u001b[32;1m"
+        z=random.choice(b)
+        print(green+x.center(z))
+        time.sleep(0.05)
